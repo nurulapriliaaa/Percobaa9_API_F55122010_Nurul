1767d522a70535d9c3ddf645729d83dd245066e9
nurulapriliaaa <nurulapriliaaa@gmail.com>
2023-11-24 21:54:59 -0800

    Initial commit

---
+++ app/src/test/java/com/nurulaprilia/percobaa9_api_f55122010_nurul/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.nurulaprilia.percobaa9_api_f55122010_nurul;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Percobaa9_API_F55122010_Nurul"
+include(":app")
